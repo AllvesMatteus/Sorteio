d497e3960420128e727a2df145e8566bdd254169
AllvesMatteus <allves.matteus@hotmail.com>
2025-02-28 17:23:14 -0300

    Atualização o README e adiciona contagem regressiva de 5 segundos ao gerar número, e um link do github, nova fonte

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.getElementById('generateButton').addEventListener('click', function() {
-    let countdown = 3;
+    let countdown = 5;
     const resultDiv = document.getElementById('result');
     
-    // Start the countdown
     const countdownInterval = setInterval(function() {
         if (countdown > 0) {
             resultDiv.textContent = `CONTAGEM REGRESSIVA: ${countdown}`;
@@ -10,7 +9,7 @@ document.getElementById('generateButton').addEventListener('click', function() {
         } else {
             clearInterval(countdownInterval);
             const randomNumber = Math.floor(Math.random() * 100) + 1;
-            resultDiv.textContent = `Número sorteado: ${randomNumber}`;
+            resultDiv.textContent = `NÚMERO SORTEADO: ${randomNumber}`;
             const numberList = document.getElementById('numberList');
             const listItem = document.createElement('li');
             listItem.textContent = randomNumber;
@@ -18,7 +17,6 @@ document.getElementById('generateButton').addEventListener('click', function() {
         }
     }, 1000);
 });
-
 
 document.getElementById('clearButton').addEventListener('click', function() {
     document.getElementById('numberList').innerHTML = '';
